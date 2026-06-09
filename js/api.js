ee0db0c74fb7f258f808b96e03f16da23ebbe4dd
nehemiah-huang <huangnehemiah10@gmail.com>
2026-06-09 11:43:13 +0000

    Server URL Update

--- js/api.js
+++ js/api.js
@@ -2,7 +2,7 @@
 
 const API = (() => {
 
-  const BASE_URL = 'http://localhost:5000/api';
+  const BASE_URL = 'seanv2-production.up.railway.app';
 
   // ── Token management ──────────────────────────────────────
   function getToken()        { return sessionStorage.getItem('medcare_token'); }
